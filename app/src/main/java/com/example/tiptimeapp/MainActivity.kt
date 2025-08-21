26bc164f86de392b057c004ccc7795017b64f89a
M.Hamed <beatlessmine@gmail.com>
2025-08-21 14:14:30 +0300

    Disable night mode and adjust insets
    The application will now default to light mode.
    Insets have been adjusted to prevent UI elements from overlapping with system bars.
    A "Proceed" action has been added to the Snackbar displayed after calculating the tip. Clicking "Proceed" will clear the input field, reset the tip amount display, and reset the tip options to their default values.
    The `totalTip` variable is now a `String` to better reflect its usage for display purposes.

--- app/src/main/java/com/example/tiptimeapp/MainActivity.kt
+++ app/src/main/java/com/example/tiptimeapp/MainActivity.kt
@@ -4,38 +4,46 @@ import android.os.Bundle
 import android.os.PersistableBundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updatePadding
 import com.example.tiptimeapp.databinding.ActivityMainBinding
 import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private var totalTip = 0.0
+    private lateinit var totalTip: String
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         binding = ActivityMainBinding.inflate(layoutInflater)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         setContentView(binding.root)
-        
-        totalTip = savedInstanceState?.getDouble("tip") ?: 0.0
+
+
+        setupInsets()
+
+
+        totalTip = savedInstanceState?.getString("tip") ?: "Tip Amount"
         binding.tipAmountLabel.text = totalTip.toString()
         /*binding.tipAmountLabel.text = totalTip.toString()
         binding.costOfServiceEditText.setText(savedInstanceState?.getString("cost"))
         binding.roundUpSwitch.isChecked = savedInstanceState?.getBoolean("roundUp") ?: false*/
-        
+
         binding.calculateTipButton.setOnClickListener { calculateTip() }
-        
+
     }
-    
+
     private fun calculateTip() {
         val stringInTextField = binding.costOfServiceEditText.text.toString()
         val cost = stringInTextField.toDoubleOrNull()
         if (cost == null) {
-            binding.tipAmountLabel.text = ""
+            "Tip Amount".also { binding.tipAmountLabel.text = it }
             return
         }
         val selectedId = binding.tipOptions.checkedRadioButtonId
@@ -50,11 +58,19 @@ class MainActivity : AppCompatActivity() {
         }
         val totalTip = "%.2f".format(tip)
         binding.tipAmountLabel.text = buildString {
-        append("$ ")
-        append(totalTip)
-    }
-        
-        Snackbar.make(binding.calculateTipButton, "Tip Amount: $totalTip", Snackbar.LENGTH_LONG).show()
+            append("$ ")
+            append(totalTip)
+        }
+
+        Snackbar.make(binding.calculateTipButton, "Tip Amount: $totalTip", Snackbar.LENGTH_LONG)
+            .setAction("Proceed", {
+                binding.costOfServiceEditText.text?.clear()
+                "Tip Amount".also { binding.tipAmountLabel.text = it }
+                binding.roundUpSwitch.isChecked = true
+                binding.amazing.isChecked = true
+                binding.tipOptions.check(R.id.amazing)
+
+            }).show()
     }
 
     override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle) {
@@ -63,5 +79,22 @@ class MainActivity : AppCompatActivity() {
         /*outState.putString("cost", binding.costOfServiceEditText.text.toString())
         outState.putBoolean("roundUp", binding.roundUpSwitch.isChecked)*/
     }
-    
+
+
+    private fun setupInsets() {
+        ViewCompat.setOnApplyWindowInsetsListener(window.decorView) { v, windowInsets ->
+            val insets = windowInsets.getInsets(WindowInsetsCompat.Type.statusBars())
+            v.updatePadding(insets.left, insets.top, insets.right, insets.bottom)
+            WindowCompat.getInsetsController(window, window.decorView).apply {
+                isAppearanceLightStatusBars = true
+            }
+
+            val systemBars = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            WindowInsetsCompat.CONSUMED
+
+        }
+
+    }
+
 }
